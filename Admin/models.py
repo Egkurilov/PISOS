c6ae3eb9efab4f21472cb14c0c90c25747eaf793
egkur <egkurilov@gmail.com>
2022-03-20 20:44:08 +0300

    edit model

--- Admin/models.py
+++ Admin/models.py
@@ -19,3 +19,42 @@ class Task(models.Model):
     name = models.CharField(max_length=255, default=None)
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     enable = models.CharField(max_length=255, default=None, blank=True, null=True)
+
+
+class ProjectList (models.Model):
+    CI = models.CharField(max_length=255, default="CI0000000", blank=True, null=True)
+    SubSystem = models.CharField(max_length=255, default="mySubsystem", blank=True, null=True)
+    Fp_name = models.CharField(max_length=255, default="DEBET_CARD", blank=True, null=True)
+
+
+class SettingsDelay(models.Model):
+    name_of_system = models.CharField(max_length=255, default="CI0000000", blank=True, null=True)
+    name_of_services = models.CharField(max_length=255, default=None, blank=True, null=True)
+    delay_URL = models.CharField(max_length=255, default="http://localhost:8080/", blank=True, null=True)
+    default_ms = models.CharField(max_length=255, default=1, blank=True, null=True)
+    critical_ms = models.CharField(max_length=255, default=2.8, blank=True, null=True)
+
+
+class SettingsCancelExternalApi(models.Model):
+    name_of_system = models.CharField(max_length=255, default=None, blank=True, null=True)
+    name_of_services = models.CharField(max_length=255, default=None, blank=True, null=True)
+    delay_URL = models.CharField(max_length=255, default="http://localhost:8080/", blank=True, null=True)
+    default_ms = models.CharField(max_length=255, default=1, blank=True, null=True)
+    critical_ms = models.CharField(max_length=255, default=60, blank=True, null=True)
+
+
+
+"""
+Тесты -> модель
+    Включить заглушку
+    Выключить заглушку
+    + Установить долгий отклик
+    Остановить сервера WAS (Фича, добавить галочку остановить один сервер) 
+    Запустить сервера WAS
+    Отключить proxy Внешней системы
+    Включить proxy Внешней системы
+    Наполнить MQ очередь
+    Очистить MQ очередь
+    Запустить тест Jmeter
+    Остановить тест Jmeter
+"""
