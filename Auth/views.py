db61aac036b8aa3b3fe1fe83b2846e6de1f668e7
egkur <egkurilov@gmail.com>
2022-05-11 20:31:13 +0300

    fix redirect

--- Auth/views.py
+++ Auth/views.py
@@ -16,15 +16,15 @@ class LoginView(View):
     def post(self, request):
         login = request.POST.get('login')
         password = request.POST.get('password')
+
         if login and password:
             login = re.match(r'[а-яА-Яa-zA-Z0-9]*', login)
             password = re.match(r'[а-яА-Яa-zA-Z0-9]*', password)
         if login.group(0) and password.group(0):
             if not User.objects.values().filter(user_login=login.group(0)):
                 return render(request, 'auth/registration.html', context={'error': 'Пользователя не существует'})
-            print("1111")
             request.session['permission'] = '001'
-            return render(request, 'square/Square.html')
+            return redirect('/cp')
 
 
 class RegisterView(View):
@@ -43,6 +43,7 @@ class RegisterView(View):
             if User.objects.values().filter(user_login=login.group(0)):
                 return render(request, 'auth/registration.html', context={'error': 'пользователь существует'})
             User.objects.create(user_login=login.group(0), password=password.group(0), permission='001')
-            request.session['permission'] = '001'
-            return render(request, 'auth/login.html')
+            request.session['login'] = login.group(0)
+            request.session['permission'] = '1'
+            return redirect('/login')
         return render(request, 'auth/registration.html')
